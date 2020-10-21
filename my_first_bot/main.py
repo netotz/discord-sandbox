dfd3a09a20b2446f3423353b0ad3443d264ed8dd
netotz <neto.otz@hotmail.com>
2020-10-21 14:30:32 -0500

    Wrap client methods in subclass

--- my_first_bot/main.py
+++ my_first_bot/main.py
@@ -7,22 +7,16 @@ import os
 import discord
 import dotenv
 
-def init_client():
-    client = discord.Client()
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged as {self.user}')
 
-    @client.event
-    async def on_ready():
-        print(f'Logged as {client.user}')
-
-    @client.event
-    async def on_message(message):
+    async def on_message(self, message):
         if message.content.startswith('hey!'):
             await message.channel.send(f'Hey @{message.author}!')
-
-    return client
 
 # bot token is on a .env file, not commited to git
 dotenv.load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 
-init_client().run(TOKEN)
+MyClient().run(TOKEN)
