5f2da99ac5a2252d5d3515389bdaee4d16bfe268
netotz <neto.otz@hotmail.com>
2020-10-26 18:02:13 -0600

    Use function to create embed instead of subclassing

--- my_first_bot/helpcmd.py
+++ my_first_bot/helpcmd.py
@@ -3,33 +3,34 @@ from discord import Embed, Colour
 from discord.ext.commands import HelpCommand, Context, Command
 from emoji import EMOJI_ALIAS_UNICODE as EMOJIS
 
-class HelpEmbed(Embed):
-    def __init__(self, context: Context):
-        questionmark = EMOJIS[':grey_question:']
-        super().__init__(
-            title=f'{questionmark} Help',
-            description="I'm a learning bot. These are my commands:",
-            colour=Colour.red(),
-            url='https://github.com/netotz/discord-sandbox'
+def create_help_embed(context: Context):
+    questionmark = EMOJIS[':grey_question:']
+    embed = Embed(
+        title=f'{questionmark} Help',
+        description="I'm a learning bot. These are my commands:",
+        colour=Colour.red(),
+        url='https://github.com/netotz/discord-sandbox'
+    )
+
+    embed.set_thumbnail(url='http://iconbug.com/data/26/256/a2ccff2488d35d8ebc6189ea693cb4a0.png')
+    embed.set_footer(text='Page 1')
+
+    command: Command
+    for command in context.bot.commands:
+        if command.hidden:
+            continue
+        embed.add_field(
+            name=command.name,
+            value=command.help,
+            inline=False
         )
 
-        self.set_thumbnail(url='http://iconbug.com/data/26/256/a2ccff2488d35d8ebc6189ea693cb4a0.png')
-        self.set_footer(text='Page 1')
-
-        command: Command
-        for command in context.bot.commands:
-            if command.hidden:
-                continue
-            self.add_field(
-                name=command.name,
-                value=command.help,
-                inline=False
-            )
+    return embed
 
 class MyHelpCommand(HelpCommand):
     async def send_bot_help(self, mapping):
         ctx: Context = self.context
 
-        embed = HelpEmbed(ctx)
+        embed = create_help_embed(ctx)
 
         await ctx.send(embed=embed)
